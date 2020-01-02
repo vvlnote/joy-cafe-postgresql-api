727149395f142abc66ad38e541598170258470c7
Vivvie Lin <vvlnote@hotmail.com>
2020-01-02 12:21:49 -0800

    add the database relation into the model folder, they are dish.rb, dish_ingredient.rb, and ingredient.rb

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,9 @@
 class Dish < ApplicationRecord
+	has_many :dish_ingredients
+	has_many :ingredients, through: :dish_ingredients
+
+	def update_dish_order_count(orders)
+		self.total_orders = self.total_orders + orders
+		self.save
+	end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,17 @@
 class Ingredient < ApplicationRecord
+	has_many :dish_ingredients
+	has_many :dishes, through: :dish_ingredients
+
+	def update_usage(amount)
+		self.used_amount = self.used_amount + amount
+		self.available_amount = self.available_amount - amount
+		if self.available_amount <= 0 || self.available_amount <= self.low_amount_alert
+			self.alert = true
+			self.save
+		else
+			self.alert = false
+			self.save
+		end
+		
+	end
 end
